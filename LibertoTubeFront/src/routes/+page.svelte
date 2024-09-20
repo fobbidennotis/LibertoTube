a4967a03d87f8410f1eacf2fd42cabab4f2c3cd7
NotisFobbidden <fobbidennotis@gmail.com>
2024-09-20 19:10:37 +0200

    Show proxy based on their status

---
+++ LibertoTubeFront/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import StatusItem from './StatusItem.svelte';
+
+  let proxy_status_data = {
+    "gfbrowser.com": "availible",
+    "down_example.com": "down"
+  };
+</script>
+
+<div class="container mx-auto text-center">
+  <h2 class="mt-5">List of available alternative domains:</h2>
+  <div class="container">
+    <div class="col">
+      {#each Object.entries(proxy_status_data) as [resource, status]}
+        <StatusItem {resource} {status} />
+      {/each}
+    </div>
+  </div>
+</div>
---
+++ LibertoTubeFront/src/routes/StatusItem.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let resource: string;
+  export let status: string;
+</script>
+
+<div class="col">
+  {resource} | <span class="{status === 'down' ? 'text-danger' : 'text-success'}">{status}</span>
+</div>
